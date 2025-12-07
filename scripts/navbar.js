26fe3f569e75210081cd49072f5ca3b6f8ad44d4
Detsel <illhacku.hah@gmail.com>
2025-12-07 16:14:07 +0300

    initial commit

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const menu = document.querySelector('#menu');
+    
+    navToggle.addEventListener('click', function() {
+        menu.classList.toggle('active-nav');
+    });
+});
